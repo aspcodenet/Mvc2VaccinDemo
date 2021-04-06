fa68481488cc623e2b85e583eb4773431d659ca7
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-04-06 14:23:01 +0200

    Add project files.

---
+++ Mvc1VaccinDemo/Controllers/PersonController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mvc1VaccinDemo.Data;
+using Mvc1VaccinDemo.Services.Krisinformation;
+using Mvc1VaccinDemo.Services.PersonGenerator;
+using Mvc1VaccinDemo.ViewModels;
+
+namespace Mvc1VaccinDemo.Controllers
+{
+    public class PersonController : BaseController
+    {
+        private readonly IPersonGeneratorService _personGeneratorService;
+
+        public PersonController(ApplicationDbContext dbContext, IKrisInfoService krisInfoService,
+            IPersonGeneratorService personGeneratorService)
+            : base(dbContext, krisInfoService)
+        {
+            _personGeneratorService = personGeneratorService;
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult BulkAdd()
+        {
+            var viewModel = new PersonBulkAddViewModel();
+            viewModel.Antal = 0;
+            return View(viewModel);
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult BulkAdd(PersonBulkAddViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                for (int i = 0; i < viewModel.Antal; i++)
+                {
+                    var generatedPerson = _personGeneratorService.GenerateFakePerson();
+                    _dbContext.Personer.Add(new Person
+                    {
+                        City = generatedPerson.City,
+                        EmailAddress = generatedPerson.EmailAddress,
+                        Name = generatedPerson.Name,
+                        PersonalNumber = generatedPerson.PersonalNumber,
+                        PostalCode = generatedPerson.PostalCode,
+                        StreetAddress = generatedPerson.StreetAddress
+                    });
+                }
+
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(viewModel);
+        }
+
+
+        // GET
+        [Authorize(Roles = "Admin, Nurse")]
+        public IActionResult Index(string q)
+        {
+           var viewModel = new PersonIndexViewModel();
+
+            viewModel.Personer = _dbContext.Personer
+                .Where(r => q == null || r.Name.Contains(q) || r.PersonalNumber.Contains(q))
+                .Select(person => new PersonViewModel
+                {
+                    Id = person.Id,
+                    Name = person.Name,
+                    EmailAddress = person.EmailAddress,
+                    PersonalNumber = person.PersonalNumber
+                }).ToList();
+
+            return View(viewModel);
+        }
+
+
+        [Authorize(Roles = "Admin, Nurse")]
+        public IActionResult _SelectPerson(int id)
+        {
+            var viewModel = new SelectPersonViewModel();
+            var p = _dbContext.Personer.Include(p=>p.VaccineringsFas).First(r => r.Id == id);
+
+            viewModel.Name = p.Name;
+            viewModel.PersonalNumber = p.PersonalNumber;
+            viewModel.NextVaccinDate = p.PreliminaryNextVaccinDate.HasValue
+                ? p.PreliminaryNextVaccinDate.Value.ToString("yyyy-MM-dd")
+                : "";
+            viewModel.VaccineringsFas = p.VaccineringsFas.Name;
+            return View(viewModel);
+        }
+
+
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Edit(int Id)
+        {
+            var viewModel = new PersonEditViewModel();
+
+            var dbPerson = _dbContext.Personer.Include(r=>r.VaccineringsFas).First(r => r.Id == Id);
+
+            viewModel.Id = dbPerson.Id;
+            viewModel.Name = dbPerson.Name;
+            viewModel.EmailAddress = dbPerson.EmailAddress;
+            viewModel.PersonalNumber = dbPerson.PersonalNumber;
+            viewModel.City = dbPerson.City;
+            viewModel.PostalCode = dbPerson.PostalCode;
+            viewModel.PreliminaryNextVaccinDate = dbPerson.PreliminaryNextVaccinDate;
+            viewModel.StreetAddress = dbPerson.StreetAddress;
+            
+            viewModel.SelectedVaccineringsFasId = dbPerson.VaccineringsFas.Id;
+            viewModel.AllaVaccineringsFaser = GetAllVaccineringsFaserAsListItems(); 
+
+            return View(viewModel);
+        }
+
+        private List<SelectListItem> GetAllVaccineringsFaserAsListItems()
+        {
+            var list = new List<SelectListItem>();
+            list.AddRange(_dbContext.VaccineringsFaser.Select(r=>new SelectListItem
+            {
+                Value = r.Id.ToString(),
+                Text = r.Name
+            }));
+            return list;
+
+        }
+    }
+}
---
+++ Mvc1VaccinDemo/Services/PersonGenerator/IPersonGeneratorService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Mvc1VaccinDemo.Services.PersonGenerator
+{
+
+    public class GeneratedPerson
+    {
+        public string Name { get; set; }
+        public string PersonalNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+
+    }
+
+    public class NameFakePerson
+    {
+        public string Name { get; set; }
+        public string Birth_data { get; set; }
+        public string email_u { get; set; }
+        public string email_d { get; set; }
+        public string Address { get; set; }
+
+
+
+    }
+
+    public interface IPersonGeneratorService
+    {
+        GeneratedPerson GenerateFakePerson();
+    }
+
+    class PersonGeneratorService : IPersonGeneratorService
+    {
+        public GeneratedPerson GenerateFakePerson()
+        {
+            var client = new HttpClient();
+            string result = client.GetStringAsync("https://api.namefake.com/swedish-sweden/random/").Result;
+            var p = JsonConvert.DeserializeObject<NameFakePerson>(result);
+            return new GeneratedPerson
+            {
+                Name = p.Name,
+                PersonalNumber = GeneratePersonalNumber(p.Birth_data),
+                EmailAddress = p.email_u + "@" + p.email_d,
+                City = p.Address.Split('\n')[1].Substring(7),
+                PostalCode = Convert.ToInt32(p.Address.Split('\n')[1].Substring(0, 6).Replace(" ", "")),
+                StreetAddress = p.Address.Split('\n')[0]
+            };
+        }
+
+        private string GeneratePersonalNumber(string Birth_data)
+        {
+            //1972-08-03
+            return Birth_data.Substring(0, 4) + Birth_data.Substring(5, 2) + Birth_data.Substring(8, 2) +
+                   "-1111";
+        }
+    }
+}
