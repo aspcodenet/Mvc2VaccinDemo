58e6aa0a141b1adea08bf64449a31c2651933012
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-04-26 10:39:36 +0200

    Added login files and google login

---
+++ Mvc1VaccinDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Mvc1VaccinDemo.Data;
+
+[assembly: HostingStartup(typeof(Mvc1VaccinDemo.Areas.Identity.IdentityHostingStartup))]
+namespace Mvc1VaccinDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Mvc1VaccinDemo/Startup.cs
+++ Mvc1VaccinDemo/Startup.cs
@@ -52,6 +52,18 @@ namespace Mvc1VaccinDemo
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    IConfigurationSection googleAuthNSection =
+                        Configuration.GetSection("Authentication:Google");
+
+                    options.ClientId = googleAuthNSection["ClientId"];
+                    options.ClientSecret = googleAuthNSection["ClientSecret"];
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
